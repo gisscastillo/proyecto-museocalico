d7903a52cddb2aa62ef0e55e8a7c7d66305d92ba
gisscastillo <gabicaztillo2670@gmail.com>
2026-02-02 21:47:43 -0600

    db.js

--- db.js
+++ db.js
@@ -1,18 +1,6 @@
 const { Pool } = require('pg');
-require('dotenv').config();
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
-pool.on('connect', () => {
-  console.log('Conexión exitosa a la base de datos');
-});
-
-pool.on('error', (err) => {
-  console.error('Error inesperado en el pool de la DB:', err);
-});
-
 module.exports = pool;
