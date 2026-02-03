3451749eff6b2f5cdb5b1d4c087dd89875e5b90a
gisscastillo <gabicaztillo2670@gmail.com>
2026-02-02 19:35:23 -0600

    productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -1,52 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); 
-const auth = require('../middleware/authMiddleware'); 
+const pool = require('../db');
 
-// Leer reservaciones
-router.get('/', auth, (req, res) => {
-    db.query('SELECT * FROM productos', (err, results) => {
-        if (err) return res.status(500).json({ estado: "Error", mensaje: "Error al leer reservaciones", error: err.message });
-        res.json(results);
+router.get('/', (req, res) => {
+    pool.query('SELECT * FROM productos', (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(result.rows);
     });
 });
 
-// Craer reservación
-router.post('/', auth, (req, res) => {
+router.post('/', (req, res) => {
     const { nombre, descripcion, precio } = req.body;
-    
-    if (!nombre || !precio) {
-        return res.status(400).json({ estado: "Error", mensaje: "Faltan campos obligatorios: nombre y cantidad (precio)" });
-    }
-
-    const query = 'INSERT INTO productos (nombre, descripcion, precio) VALUES (?, ?, ?)';
-    db.query(query, [nombre, descripcion, precio], (err, result) => {
-        if (err) return res.status(500).json({ estado: "Error", error: err.message });
-        res.status(201).json({ mensaje: 'Reservación creada con éxito', id: result.insertId });
+    const sql = 'INSERT INTO productos (nombre, descripcion, precio) VALUES ($1, $2, $3)';
+    pool.query(sql, [nombre, descripcion, precio], (err) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.status(201).json({ mensaje: "Producto creado" });
     });
 });
-
-// Actualizar
-router.put('/:id', auth, (req, res) => {
-    const { id } = req.params;
-    const { nombre, descripcion, precio } = req.body;
-
-    const query = 'UPDATE productos SET nombre = ?, descripcion = ?, precio = ? WHERE id = ?';
-    db.query(query, [nombre, descripcion, precio, id], (err, result) => {
-        if (err) return res.status(500).json({ estado: "Error", error: err.message });
-        if (result.affectedRows === 0) return res.status(404).json({ mensaje: "Registro no encontrado" });
-        res.json({ mensaje: 'Registro actualizado correctamente' });
-    });
-});
-
-// Eliminar
-router.delete('/:id', auth, (req, res) => {
-    const { id } = req.params;
-    db.query('DELETE FROM productos WHERE id = ?', [id], (err, result) => {
-        if (err) return res.status(500).json({ estado: "Error", error: err.message });
-        if (result.affectedRows === 0) return res.status(404).json({ mensaje: "Registro no encontrado" });
-        res.json({ mensaje: 'Registro eliminado del sistema' });
-    });
-});
-
 module.exports = router;
