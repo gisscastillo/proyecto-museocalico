4ff2cbef1c1271fa2c3d9d9c95b633d68af36abd
gisscastillo <gabicaztillo2670@gmail.com>
2026-02-02 19:29:06 -0600

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const db = require('../db');
+const pool = require('../db'); 
 const bcrypt = require('bcryptjs');
 
 // Registro
@@ -11,13 +11,12 @@ router.post('/register', async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Verifica que los nombres de las columnas coincidan 
-        const sql = 'INSERT INTO usuarios (nombre, email, password) VALUES (?, ?, ?)';
         
-        db.query(sql, [nombre, email, hashedPassword], (err, result) => {
+        const sql = 'INSERT INTO usuarios (nombre, email, password) VALUES ($1, $2, $3)';
+        
+        pool.query(sql, [nombre, email, hashedPassword], (err, result) => {
             if (err) {
-                console.error("DETALLE ERROR MYSQL:", err); 
+                console.error("DETALLE ERROR POSTGRES:", err); 
                 return res.status(500).json({ mensaje: "Error al guardar en base de datos" });
             }
             res.status(201).json({ mensaje: "Usuario registrado con éxito" });
@@ -31,16 +30,17 @@ router.post('/register', async (req, res) => {
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
 
-    db.query('SELECT * FROM usuarios WHERE email = ?', [email], async (err, results) => {
+    pool.query('SELECT * FROM usuarios WHERE email = $1', [email], async (err, result) => {
         if (err) return res.status(500).json({ error: err.message });
-        if (results.length === 0) return res.status(400).json({ mensaje: "Usuario no encontrado" });
+        
+        if (result.rows.length === 0) return res.status(400).json({ mensaje: "Usuario no encontrado" });
 
-        const usuario = results[0];
+        const usuario = result.rows[0];
         const validPass = await bcrypt.compare(password, usuario.password);
         
         if (!validPass) return res.status(400).json({ mensaje: "Contraseña incorrecta" });
 
-        const token = jwt.sign({ id: usuario.id }, process.env.JWT_SECRET, { expiresIn: '2h' });
+        const token = jwt.sign({ id: usuario.id }, process.env.JWT_SECRET || 'clave_secreta', { expiresIn: '2h' });
 
         res.json({ mensaje: "Login exitoso", token: token });
     });
